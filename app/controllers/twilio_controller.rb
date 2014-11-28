6b0d881a17405734bd248fb039ea522468cd7236
vadivelan <reachme.vadivelan@gmail.com>
2014-11-28 08:37:05 +0530

    First commit::App structure pushed

---
+++ app/controllers/twilio_controller.rb
@@ -0,0 +1,8 @@
+class TwilioController < ApplicationController
+	def voice_callback
+		puts params.inspect
+		respond_to do |format|
+			format.xml
+		end
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmartReminder::Application.config.secret_token = '0d747f1fd9982cfcf7390a5c97e10e9710b8785667b9b4e92cb868487682ada3f10d642351c5bbc8d110249979a5198f0fa5aa7e60d059faaf47cc325cc3cc3f'
